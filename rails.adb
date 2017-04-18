30289162e74c1e9ecbbc25565b95356bd9c1f720
afrometal <radek.kwlsk@gmail.com>
2017-04-18 10:29:45 +0200

    tracks package

---
+++ rails.adb
@@ -0,0 +1,95 @@
+package body Rails is
+
+    protected body Track is
+        function Get_Id return Integer is
+        begin
+            return Id;
+        end Get_Id;
+
+        function Get_Type return Track_Type is
+        begin
+            return Typee;
+        end Get_Type;
+
+        entry Get_Lock(Suc : out Boolean) when TRUE is
+        begin
+            case Locked is
+                when TRUE =>
+                    Suc := FALSE;
+                when FALSE =>
+                    Locked := TRUE;
+                    Suc := TRUE;
+            end case;
+        end Get_Lock;
+
+        entry Lock when not Locked is
+        begin
+            Locked := TRUE;
+        end Lock;
+
+        entry Unlock when Locked is
+        begin
+            Locked := FALSE;
+        end Unlock;
+
+        function As_String return String is
+        begin
+            case Typee is
+                when Turntable =>
+                    return "Turntable" & Integer'Image (Id);
+                when Normal =>
+                    return "NormalTrack" & Integer'Image (Id);
+                when Station =>
+                    return "StationTrack" & Integer'Image (Id) & " " & SU.To_String (Spec.Name);
+            end case;
+        end As_String;
+
+        function Action_Time(Train_Speed : Integer) return Float is
+        begin
+            case Typee is
+                when Turntable =>
+                    return Float (Spec.Rotation_Time) / 60.0;
+                when Normal =>
+                    return Float (Spec.Length) / Float (Integer'Min(Spec.Speed_Limit, Train_Speed));
+                when Station =>
+                    return Float (Spec.Stop_Time) / 60.0;
+            end case;
+        end Action_Time;
+
+        procedure Init (I : in Integer; S : in Track_Record) is
+        begin
+            Id := I;
+            Spec := S;
+        end Init;
+    end Track;
+
+    function New_Turntable(I : Integer; T : Integer) return Track_Ptr is
+        TP : Track_Ptr;
+        S : Track_Record;
+    begin
+        TP := new Track(Turntable);
+        S :=  (Typee => Turntable, Rotation_Time => T);
+        TP.Init(I, S);
+        return TP;
+    end New_Turntable;
+
+    function New_Normal_Track(I : Integer; L : Integer; SL : Integer) return Track_Ptr is
+        TP : Track_Ptr;
+        S : Track_Record;
+    begin
+        TP := new Track(Normal);
+        S :=  (Typee => Normal, Length => L, Speed_Limit => SL);
+        TP.Init(I, S);
+        return TP;
+    end New_Normal_Track;
+
+    function New_Station_Track(I : Integer; T : Integer; N : SU.Unbounded_String) return Track_Ptr is
+        TP : Track_Ptr;
+        S : Track_Record;
+    begin
+        TP := new Track(Station);
+        S :=  (Typee => Station, Stop_Time => T, Name => N);
+        TP.Init(I, S);
+        return TP;
+    end New_Station_Track;
+end Rails;
---
+++ rails.ads
@@ -0,0 +1,66 @@
+--with Ada.Text_IO;                use Ada.Text_IO;
+with Ada.Strings.Unbounded;
+
+package Rails is
+    package SU renames Ada.Strings.Unbounded;
+
+    type Track_Type is (Turntable, Normal, Station);
+
+    type Track_Record (Typee: Track_Type := Turntable) is
+    record
+        case Typee is
+            when Turntable =>
+                Rotation_Time : Integer;
+            when Normal =>
+                Length : Integer;
+                Speed_Limit : Integer;
+            when Station =>
+                Stop_Time : Integer;
+                Name : SU.Unbounded_String;
+        end case;
+    end record;
+
+    protected type Track (T : Track_Type) is
+        function Get_Id return Integer;
+        function Get_Type return Track_Type;
+        entry Get_Lock(Suc : out Boolean);
+        entry Lock;
+        entry Unlock;
+        function As_String return String;
+        function Action_Time(Train_Speed : Integer) return Float;
+        procedure Init (I : in Integer; S : in Track_Record);
+    private
+        Id : Integer;
+        Locked : Boolean := FALSE;
+        Typee : Track_Type := T;
+        Spec : Track_Record (T);
+    end Track;
+
+    type Track_Ptr is access Track;
+
+    type Tracks_Array is array(Integer range <>) of Track_Ptr;
+    type Tracks_Ptr is access Tracks_Array;
+
+    type Route_Array is new Tracks_Array;
+    type Route_Ptr is access Route_Array;
+
+    function New_Turntable(I : Integer; T : Integer) return Track_Ptr;
+
+    function New_Normal_Track(I : Integer; L : Integer; SL : Integer) return Track_Ptr;
+
+    function New_Station_Track(I : Integer; T : Integer; N : SU.Unbounded_String) return Track_Ptr;
+
+    type Turntables_Array is new Tracks_Array;
+    type Turntables_Ptr is access Turntables_Array;
+
+    type Normal_Tracks_Array is new Tracks_Array;
+    type Normal_Tracks_Ptr is access Normal_Tracks_Array;
+
+    type Station_Tracks_Array is new Tracks_Array;
+    type Station_Tracks_Ptr is access Station_Tracks_Array;
+
+    type Connections_Array is array(Integer range <>, Integer range <>) of Tracks_Ptr;
+
+    type Connections_Ptr is access Connections_Array;
+
+end Rails;
