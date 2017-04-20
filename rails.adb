db57fe83ac3f14b858c4eb7f21645986a519be94
afrometal <radek.kwlsk@gmail.com>
2017-04-21 00:29:59 +0200

    preety printing

--- rails.adb
+++ rails.adb
@@ -44,6 +44,22 @@ package body Rails is
             end case;
         end As_String;
 
+        function As_Verbose_String return String is
+            Id : String := Integer'Image (Get_Id);
+        begin
+            case Typee is
+                when Turntable =>
+                    return "rails.Turntable:" & Id (2 .. Id'Last) & "{time:" &
+                    Integer'Image (Spec.Rotation_Time) & "}";
+                when Normal =>
+                    return "rails.NormalTrack:" & Id (2 .. Id'Last) & "{len:" &
+                    Integer'Image (Spec.Length) & ", limit:" & Integer'Image (Spec.Speed_Limit) & "}";
+                when Station =>
+                    return "rails.StationTrack:" & Id (2 .. Id'Last) & ":" & SU.To_String (Spec.Name) & "{time:" &
+                    Integer'Image (Spec.Stop_Time) & "}";
+            end case;
+        end As_Verbose_String;
+
         function Action_Time(Train_Speed : Integer) return Float is
         begin
             case Typee is
@@ -65,7 +81,7 @@ package body Rails is
 
     function New_Turntable(I : Integer; T : Integer) return Track_Ptr is
         TP : Track_Ptr;
-        S : Track_Record;
+        S  : Track_Record;
     begin
         TP := new Track(Turntable);
         S :=  (Typee => Turntable, Rotation_Time => T);
@@ -75,7 +91,7 @@ package body Rails is
 
     function New_Normal_Track(I : Integer; L : Integer; SL : Integer) return Track_Ptr is
         TP : Track_Ptr;
-        S : Track_Record;
+        S  : Track_Record;
     begin
         TP := new Track(Normal);
         S :=  (Typee => Normal, Length => L, Speed_Limit => SL);
@@ -85,11 +101,24 @@ package body Rails is
 
     function New_Station_Track(I : Integer; T : Integer; N : SU.Unbounded_String) return Track_Ptr is
         TP : Track_Ptr;
-        S : Track_Record;
+        S  : Track_Record;
     begin
         TP := new Track(Station);
         S :=  (Typee => Station, Stop_Time => T, Name => N);
         TP.Init(I, S);
         return TP;
     end New_Station_Track;
+
+
+    function As_String(Self: Route_Array) return String is
+        S : SU.Unbounded_String;
+    begin
+        S := SU.To_Unbounded_String ("[");
+        for I in Self'range loop
+                SU.Append(S, Integer'Image (I) (2 .. Integer'Image (I)'Last) & " ");
+        end loop;
+        SU.Append(S, "]");
+
+        return SU.To_String (S);
+    end As_String;
 end Rails;
